9fe2237641bbc254f4b77b434f091077bf3897d4
Philipe Perez <philipeperez@gmail.com>
2026-03-30 20:14:24 -0400

    Change response for emotion_detector

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze: str):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -11,5 +12,16 @@ def emotion_detector(text_to_analyze: str):
         'grpc-metadata-mm-model-id': 'emotion_aggregated-workflow_lang_en_stock'
     }
     response = requests.post(url, json=myobj, headers=header)
+    formated_response = json.loads(response.text)
 
-    return response.text
+    emotion_predictions = formated_response['emotionPredictions'][0]['emotion']
+
+    res = {**emotion_predictions, 'dominant_emotion': ''}
+    greatest_emotion_value = 0
+
+    for k in emotion_predictions:
+        if emotion_predictions[k] > greatest_emotion_value:
+            greatest_emotion_value = emotion_predictions[k]
+            res['dominant_emotion'] = k
+
+    return res
